cae257c99ecf60bbb496d05cb782da8ce7e3d2ae
BebeShen <55395848+BebeShen@users.noreply.github.com>
2020-12-20 15:45:45 +0800

    [Test] change parser to handler

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def callback():
 
     # handle webhook body
     try:
-        events = parser.parse(body, signature)
+        handler.handle(body, signature)
     except InvalidSignatureError:
         print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
